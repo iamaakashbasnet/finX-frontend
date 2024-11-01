cca1c15122dff9a9b0e705eef1916d71bdee446d
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-11-01 15:30:28 +0545

    chore: mantine ui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { createTheme, MantineProvider } from '@mantine/core';
 import { RouterProvider } from 'react-router-dom';
 
 import router from 'routers/router';
 
+const theme = createTheme({
+  fontFamily:
+    "Inter, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+});
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="light">
+      <RouterProvider router={router} />
+    </MantineProvider>
+  );
 };
 
 export default App;
