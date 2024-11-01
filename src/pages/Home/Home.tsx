38c8b8f5e46abeffd508c39a19db6e4e51461d84
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-11-01 15:46:30 +0545

    feat: home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,25 @@
+import { Image, Flex, Container, Button, Text, Title } from '@mantine/core';
+
+import logo from 'assets/logo.png';
+
 const Home = () => {
   return (
-    <>
-      <div>Home</div>
-    </>
+    <Container>
+      <Flex
+        justify="center"
+        align="center"
+        direction="column"
+        gap="md"
+        style={{ height: '100vh', textAlign: 'center' }}
+      >
+        <Image radius="md" h={200} w="auto" fit="contain" src={logo} />
+        <Title order={1} size="h1">
+          finX
+        </Title>
+        <Text>Next generation cloud based complex portfolio and wealth management software platform</Text>
+        <Button>Get Started</Button>
+      </Flex>
+    </Container>
   );
 };
 
