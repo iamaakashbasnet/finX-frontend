49dce91c20f7928d6b989da39a887c62c3819e57
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-12-23 23:04:07 +0545

    feat: basic general clients setup

--- src/pages/clients/GeneralClients/GeneralClients.tsx
+++ src/pages/clients/GeneralClients/GeneralClients.tsx
@@ -1,16 +1,72 @@
-import { Text } from '@mantine/core';
+import { useQuery } from '@tanstack/react-query';
+import { Space, Table, Text, Modal, Paper } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { LuUserRoundCheck, LuUserRoundX } from 'react-icons/lu';
 
 import { fetchGeneralClients } from './api';
-import { useEffect } from 'react';
 
 export default function GeneralClients() {
-  useEffect(() => {
-    fetchGeneralClients();
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['fetch-general-clients'],
+    queryFn: () => fetchGeneralClients(),
   });
 
   return (
     <>
-      <Text size="lg">General Clients</Text>
+      <Text size="lg" fw="bold">
+        General Clients
+      </Text>
+      <Text size="sm" c="dimmed" pl="sm">
+        General client's portfolio are managed separately from pool investments
+      </Text>
+      <Space h="lg" />
+      {isLoading && <Text>Loading...</Text>}
+      <Paper
+        style={{
+          borderRadius: '.3rem',
+          overflow: 'hidden',
+        }}
+        shadow="sm"
+        withBorder
+      >
+        <Table style={{ borderRadius: '2.5rem' }} striped withTableBorder withColumnBorders highlightOnHover>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>Name</Table.Th>
+              <Table.Th>Email</Table.Th>
+              <Table.Th>Status</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {data?.map((client) => (
+              <Table.Tr key={client.user.id} onClick={open} style={{ cursor: 'pointer' }}>
+                <Table.Td>
+                  {client.user.first_name} {client.user.last_name}
+                </Table.Td>
+                <Table.Td>{client.user.email}</Table.Td>
+                <Table.Td>
+                  {client.is_active ? <LuUserRoundCheck color="green" /> : <LuUserRoundX color="red" />}
+                </Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </Paper>
+
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Clients Details"
+        size="xl"
+        overlayProps={{
+          backgroundOpacity: 0.55,
+          blur: 3,
+        }}
+      >
+        <Text>This modal will contain a form for updating clients data</Text>
+      </Modal>
     </>
   );
 }
--- src/pages/clients/GeneralClients/api/index.ts
+++ src/pages/clients/GeneralClients/api/index.ts
@@ -1,12 +1,22 @@
 import apiClient from 'api/apiClient';
 
-export const fetchGeneralClients = () => {
-  apiClient
-    .get(`/clients/general`)
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+interface GeneralClientI {
+  user: { id: number; first_name: string; last_name: string; username: string; email: string };
+  is_active: boolean;
+}
+
+interface GeneralClientsResponseI {
+  count: number;
+  next: string | null;
+  results: GeneralClientI[];
+}
+
+export const fetchGeneralClients = async () => {
+  try {
+    const res = await apiClient.get<GeneralClientsResponseI>(`/clients/general`);
+    return res.data.results;
+  } catch (err) {
+    console.error('Something went wrong:', err);
+    throw err;
+  }
 };
